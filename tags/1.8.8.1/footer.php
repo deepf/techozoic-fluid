aa7f263b79a9f8a341c62f13d587f46350994443
jeremyclark13 <jeremyclark13@010e1cd8-cc02-723b-e9b4-cf61d25fcfdd>
2010-07-06 12:35:28 +0000

    Copying trunck into branch 1.8.8 for new release version

---
+++ tags/1.8.8.1/footer.php
@@ -0,0 +1,27 @@
+<?php 
+get_tech_options();
+global $tech;
+?>
+<hr />
+<div id="footermain">
+<div id="footer">
+<div id="footerdivs">
+<?php if ( !function_exists('dynamic_sidebar')
+        || !dynamic_sidebar('Footer - Limit 3 Widgets') ) : ?>
+<?php endif; ?>
+</div>
+<div style="clear:both"></div>
+	<p class="credit">
+
+	<?php	wp_footer(); ?>
+<br /><small>
+<?php printf(__('%1$d mySQL queries in %2$s seconds.','techozoic'), get_num_queries(),timer_stop(0)); ?>
+</small>
+	</p>
+</div><!--footer-->
+</div><!--footercont"-->
+</div><!--pager-->
+</div><!--pagel-->
+</div><!--page-->
+</body>
+</html>
